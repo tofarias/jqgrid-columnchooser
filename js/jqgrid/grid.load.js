52c9f52c7f6d50a6863d083159337d15442b6826
tiago <tiago@ubuntu-desktop>
2015-01-11 15:18:58 -0200

    Add - load and sort the columns positions.

--- js/jqgrid/grid.load.js
+++ js/jqgrid/grid.load.js
@@ -13,6 +13,65 @@ $(function(){
 				width: 1000,
 				height: 250,
 				pager: pagerId,
+				gridComplete: function(){
+					
+					$.post('load-columns-position', null, function($response){
+						
+						if( $response ){
+							
+							var colModels = $table.jqGrid('getGridParam', 'colModel');
+							var $responseCols = $response.columns_position;
+							var columns = [];
+							
+							if( $responseCols.length > 0 ){
+								
+								columns[0] = 0;
+								
+								$.each($responseCols, function(icol, columnIndex){
+									
+									if( icol > 0 ){
+										
+										var pos = columnIndex.index;
+										var field = columnIndex.nm_campo;
+										
+										$.each(colModels, function(i, colModel){
+											
+											var colModelIndex = colModel.index;
+											
+											if( field == colModelIndex ){
+												
+												columns[icol] = i;
+												
+												if( $.parseJSON( columnIndex.isHidden ) ){
+													$grid.jqGrid('hideCol', [field]);
+												}
+											}
+										});
+									}
+								});
+								
+								$table.jqGrid("remapColumns", columns, true);
+								
+							}else{
+						    								    	
+								$.each(colModels, function(i, colModel){
+						    		
+						    		columns.push({
+					    				field: colModel.index,
+					    				isHidden: colModel.hidden,
+					    				pos: i
+					    			});
+						    	});
+								
+								$.post('save-columns-position',
+						    			{
+						    				cols: columns,
+						    				field: 'columns_position' // the field name in the table
+						    			}, null);
+							}
+						}
+					});
+				},
 				colNames: ['State', 'Name', 'Country', 'City', 'E-mail', 'Phone', 'Zip-Code'],
 				colModel:
 					[
@@ -58,12 +117,39 @@ $(function(){
 			}).navGrid(pagerId,{ search: false, edit: false, add: false, del: false, view:false });
 
 	$table.jqGrid('navButtonAdd',pagerId,{
-		caption: "Colunas",
+		caption: "Select Columns",
 		buttonicon: 'ui-icon-carat-2-n-s',
-		title: "Reordenar Colunas",
+		title: "Reorder Columns",
 		onClickButton : function (){
-			$table.columnChooser(null);
-		}
+	    	$table.columnChooser(
+	    			{
+	    				done: function(perm){
+	    					
+	    					if( (typeof (perm) !== "undefined") ){
+	    						
+	    						$table.jqGrid("remapColumns", perm, true);
+		    					
+						    	var colModels = $table.jqGrid('getGridParam', 'colModel');									    	
+						    	var coluns = new Array();
+
+						    	$.each(perm, function(i, columnIndex){
+						    		
+						    		columns.push({
+					    				field: colModels[i].index,
+					    				isHidden: colModels[i].hidden,
+					    				pos: columnIndex
+					    			});
+								});
+						    	
+						    	$.post('save-columns-position',
+						    			{
+						    				cols: columns,
+						    				field: 'columns_position' // the field name in the table
+						    			}, null);
+	    					}
+	    				}
+	    			});
+	    }
 	}); 
 
 });
